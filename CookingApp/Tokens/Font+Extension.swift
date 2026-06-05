595dd8f81aba6df19b09c7bb2a0c45fd3c7d4d5c
farhah <farhahnash@gmail.com>
2026-06-05 14:18:03 +0700

    font

---
+++ CookingApp/Tokens/Font+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  Font.swift
+//  CookingApp
+//
+//  Created by Farhah Nashrillah on 05/06/26.
+//
+
+import SwiftUI
+
+extension Font {
+    static let LargeTitle = Font.system(size: 34, weight: .bold)
+    static let Title = Font.system(size: 22, weight: .bold)
+    static let Headline = Font.system(size: 16, weight: .semibold)
+    static let Body = Font.system(size: 17, weight: .regular)
+    static let Footnote = Font.system(size: 13, weight: .regular)
+}
