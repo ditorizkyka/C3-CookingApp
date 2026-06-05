738d4c95076d218751e20196af665b349f8b59f2
ditorizkyka <rizkykandito@gmail.com>
2026-06-05 13:43:50 +0700

    Initial Commit

---
+++ CookingApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CookingApp
+//
+//  Created by Andito Rizkyka Rianto on 05/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CookingApp/CookingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookingAppApp.swift
+//  CookingApp
+//
+//  Created by Andito Rizkyka Rianto on 05/06/26.
+//
+
+import SwiftUI
+
+@main
+struct CookingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
